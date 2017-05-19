7074324ea2d54c868afdb7cad67c2e075027305f
annesoh <anne.soh@my.jcu.edu.au>
2017-05-19 22:23:50 +0800

    Product detail pages
    Added pages: saltwaterfish, fishfood, medications, fishtanks,
    aquariumdecorations.
    Created javascript page for image change on click function on fish
    pages.
    Some changes to css.
    Added fish image for bullet points.

---
+++ scriptdetail.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+/*freshwater fish*/
+var onImgG= "images/fish/guppy1.jpg";var offImgG= "images/fish/guppy2.jpg";
+var onImgFG= "images/fish/fantailgoldfish1.jpg";var offImgFG= "images/fish/fantailgoldfish2.jpg";
+var onImgOG= "images/fish/orandagoldfish1.jpg";var offImgOG= "images/fish/orandagoldfish2.jpg";
+var onImgNT= "images/fish/neontetra1.jpg";var offImgNT= "images/fish/neontetra2.jpg";
+
+/*saltwater fish*/
+var onImgCF= "images/fish/clownfish1.jpg";var offImgCF= "images/fish/clownfish2.jpg";
+var onImgBF= "images/fish/butterflyfish1.jpg";var offImgBF= "images/fish/butterflyfish2.jpg";
+var onImgAF= "images/fish/angelfish1.jpg";var offImgAF= "images/fish/angelfish2.jpg";
+var onImgDF= "images/fish/doryfish1.jpg";var offImgDF= "images/fish/doryfish2.jpg";
